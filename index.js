319b7cc977de55ee2175617b73ee1b464128bcbe
Neftaly <neftalyrv23@gmail.com>
2023-08-11 15:50:17 -0400

    Made animation run when button  is clicked

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 console.log("Hello js!");
+const submitButton = document.querySelector('button[type="submit"]');
+console.log(submitButton);
+submitButton.addEventListener("click", (event) => {
+  console.log("clicked!");
+  event.preventDefault();
+  addRipple(event);
+});
+
+function addRipple(event) {
+  const parent = event.target;
+
+  const span = document.createElement("span");
+
+  const max = Math.max(parent.clientWidth, parent.clientHeight);
+  const radius = max / 2;
+  span.style.width = max + "px";
+  span.style.height = max + "px";
+
+  span.style.top = `${event.clientY - parent.offsetTop - radius}px`;
+  span.style.left = `${event.clientX - parent.offsetLeft - radius}px`;
+  span.classList.add("ripple");
+  // Check if an element with ripple exists and remove it
+
+  parent.appendChild(span);
+
+  setTimeout(() => {
+    parent.removeChild(span);
+  }, 1000);
+}
+
+function calculatePosition(target) {}
